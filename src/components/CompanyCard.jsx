f6d4f277c8f291244a747a03237f5840aebb06d4
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-11-25 22:52:15 +0100

    feat: Add CompanyCard

---
+++ src/components/CompanyCard.jsx
@@ -0,0 +1,23 @@
+//AntD Components
+import { Col, Card } from 'antd';
+
+const CompanyCard = ({ company }) => {
+    return (
+        <>
+            <Col key={company.id} xs={24} sm={12} lg={6} className="company-card" span={12} >
+                <Card
+                    title={`${company.company_name}`}
+                    extra={<img className="company-logo" src={company.logo} alt={company.company_name} />}
+                    style={{ borderRadius: '3%' }}
+                    hoverable
+                >
+                    <h3>{company.company_name}</h3>
+                    <p>Specialices in: {company.specialities}</p>
+                    <p>Located at: {company.city}</p>
+                </Card>
+            </Col>
+        </>
+    )
+}
+
+export default CompanyCard;
--- src/components/CompanyList.jsx
+++ src/components/CompanyList.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
-import { Card, Row, Col, Input, Checkbox, Collapse } from 'antd';
+import { Row, Input, Checkbox, Collapse } from 'antd';
 
 import { specialityList } from '../constants';
+import CompanyCard from './CompanyCard';
 
 const { Panel } = Collapse;
 
@@ -79,18 +80,7 @@ const CompanyList = ({ companiesList }) => {
             </Collapse>
             <Row gutter={[32, 32]} className="card-container">
                 {companies?.map((company) => (
-                    <Col key={company.id} xs={24} sm={12} lg={6} className="company-card" span={12} >
-                        <Card
-                            title={`${company.company_name}`}
-                            extra={<img className="company-logo" src={company.logo} alt={company.company_name} />}
-                            style={{ borderRadius: '3%' }}
-                            hoverable
-                        >
-                            <h3>{company.company_name}</h3>
-                            <p>Specialices in: {company.specialities}</p>
-                            <p>Located at: {company.city}</p>
-                        </Card>
-                    </Col>
+                    <CompanyCard key={company.id} company={company} />
                 ))}
             </Row>
         </>
