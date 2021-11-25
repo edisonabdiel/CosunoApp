7eb4a92bd4edb6ba79cd8fa33fbcf7c1d2581a39
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-11-25 10:38:02 +0100

    feat: Add search function. Add checkboxes. style: Add clases for checkbox

--- src/components/CompanyList.jsx
+++ src/components/CompanyList.jsx
@@ -1,50 +1,102 @@
 import React, { useState, useEffect } from 'react';
 
-import { Card, Row, Col, Input } from 'antd';
+import { Card, Row, Col, Input, Checkbox, Collapse } from 'antd';
 
 import { useGetCompaniesQuery } from '../services/cosunoApi';
 
+import { specialityList } from '../constants';
+
 import Loader from './Loader';
 
+const { Panel } = Collapse;
+
 const CompanyList = () => {
-    
+
     const { data: companiesList, isFetching } = useGetCompaniesQuery();
     const [companies, setCompanies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+    const [checkedSpeciality, setCheckedSpeciality] = useState([]);
 
-    console.log('companiesList', companiesList);
-    // Gets executed whenever the dependencies array changes. 
+    console.log(checkedSpeciality)
+
+     
     useEffect(() => {
 
         const filteredCompanies = companiesList?.filter((company) => (
             company.company_name.toLowerCase().includes(searchTerm.toLowerCase()))
         );
-
+        
+        
+        
         setCompanies(filteredCompanies);
-
+        
     }, [companiesList, searchTerm]);
+    
+    useEffect(() => {
+        
+        const filteredCompaniesBySpeciality = companies?.map((company) => {
+            if (checkedSpeciality.length === 0) {
+                return company;
+            }
+            return company?.specialities.toLowerCase().some((speciality) => checkedSpeciality?.toLowerCase().includes(speciality));
+        });
+        
+
+        setCompanies(filteredCompaniesBySpeciality);
+        
+
+    }, [checkedSpeciality]);
+
+
+    const handleCheckbox = (speciality) => {
+
+        const index = checkedSpeciality.indexOf(speciality);
+        const newChecked = [...checkedSpeciality];
+
+        if (index === -1) {
+            newChecked.push(speciality);
+        } else {
+            newChecked.splice(index, 1);
+        }
+
+        setCheckedSpeciality(newChecked);
+    }
+
 
     if (isFetching) return <Loader />;
 
-
     return (
         <>
-                <div className="search">
-                    <Input placeholder="Search Construction Company" onChange={(e) => setSearchTerm(e.target.value)} />
-                </div>
+            <div className="search">
+                <Input placeholder="Search Construction Company" onChange={(e) => setSearchTerm(e.target.value)} />
+            </div>
+            <Collapse defaultActiveKey={['0']} style={{marginBottom: '25px'}}>
+                <Panel header="Filter By Speciality" key="1">
+                    <div className="checkbox">
+                        {specialityList.map((speciality, index) => (
+                            <Checkbox 
+                                key={index}
+                                value={speciality}
+                                onChange={() => handleCheckbox(speciality)}>
+                                {speciality}
+                            </Checkbox>
+                        ))}
+                    </div>
+                </Panel>
+            </Collapse>
             <Row gutter={[32, 32]} className="card-container">
                 {companies?.map((company) => (
                     <Col key={company.id} xs={24} sm={12} lg={6} className="company-card" span={12} >
-                            <Card
-                                title={`${company.company_name}`}
-                                extra={<img className="company-logo" src={company.logo} alt={company.company_name} />}
-                                style={{borderRadius: '3%'}}
-                                hoverable
-                            >
+                        <Card
+                            title={`${company.company_name}`}
+                            extra={<img className="company-logo" src={company.logo} alt={company.company_name} />}
+                            style={{ borderRadius: '3%' }}
+                            hoverable
+                        >
                             <h3>{company.company_name}</h3>
                             <p>Specialices in: {company.specialities}</p>
                             <p>Located at: {company.city}</p>
-                            </Card>
+                        </Card>
                     </Col>
                 ))}
             </Row>
---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const specialityList = [
+    'Roofing (Metal)',
+    'Frames & Hardware',
+    'Epoxy Flooring',
+    'Fire Sprinkler Systems',
+    'Overhead Doors',
+    'Wall Protection',
+    'Casework',
+    'Decorative Fencing and Gates',
+    'Drywall & Acoustical (MOB)',
+    'Drilled Shafts',
+    'Rebar & Wire Mesh Install',
+    'Plumbing & Medical Gas'
+]
